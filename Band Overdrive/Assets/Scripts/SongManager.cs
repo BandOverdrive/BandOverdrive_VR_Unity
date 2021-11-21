53ee0ab52a76801dd7d5305218f1e3b5a8a10c31
yvettemuki <373628977@qq.com>
2021-11-21 12:40:56 -0500

    feat: drywetmidi import and basic setup songManager

---
+++ Band Overdrive/Assets/Scripts/SongManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Melanchall.DryWetMidi.Core;
+using Melanchall.DryWetMidi.Interaction;
+using System.IO;
+using UnityEngine.Networking;
+
+public class SongManager : MonoBehaviour
+{
+    public static SongManager Instance;
+    public AudioSource audioSource;
+    public float songDelayInSeconds; // delay second for playing song
+    public float inputDelayInMilliseconds;  // keyboard input delay time
+
+    public string fileLocation;
+    public float noteTime;
+    public float noteSpawnY;
+    public float noteTapY;
+
+    public float noteDespawnY
+    {
+        get
+        {
+            return noteTapY - (noteSpawnY - noteTapY);
+        }
+    }
+
+    public static MidiFile midiFile; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
