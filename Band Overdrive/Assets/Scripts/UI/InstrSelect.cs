fe0bb1218a7d1c0f30e6e9dc19b0470baf6f53dd
Bearbbcjtc <bearbbcjtc@gmail.com>
2021-12-02 02:21:50 -0500

    UI updates

--- Band Overdrive/Assets/Scripts/UI/InstrSelect.cs
+++ Band Overdrive/Assets/Scripts/UI/InstrSelect.cs
@@ -1,17 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
+public class InstrSelect : MonoBehaviour
 {
-    public void OnPlayGame(int sceneIndex)
-    {
-        
-        SceneManager.LoadScene(sceneIndex);
-    
-    }
-
+    public string instrname;
 
     // Start is called before the first frame update
     void Start()
@@ -23,5 +16,11 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void OnClick()
+    {
+        instrname = this.gameObject.name;
+        Debug.Log(instrname);
     }
 }
